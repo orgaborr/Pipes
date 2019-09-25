97efafaa2df46ed270d6b811d075c944c4b6ab3f
Gabor <orbangabeszb@gmail.com>
2019-09-25 16:27:38 +0200

    comments added for description

--- src/MainPipe.java
+++ src/MainPipe.java
@@ -7,23 +7,23 @@ public class MainPipe {
 	public static void main(String[] args) {
 
 		try {
-			Pipe pipe = Pipe.open();
+			Pipe pipe = Pipe.open(); // we open the pipe
 
 			Runnable writer = () -> {
 				try {
-					Pipe.SinkChannel sinkChannel = pipe.sink();
-					ByteBuffer buffer = ByteBuffer.allocate(56);
+					Pipe.SinkChannel sinkChannel = pipe.sink(); // SinkChannel is FROM through we send data
+					ByteBuffer buffer = ByteBuffer.allocate(56); // initializing buffer for 56 bytes
 
 					for(int i=0; i<10; i++) {
 						String currentTime = "The time is " + System.currentTimeMillis();
-						buffer.put(currentTime.getBytes());
+						buffer.put(currentTime.getBytes()); // putting the byte array of the String
 						buffer.flip();
 
-						while(buffer.hasRemaining()) {
-							sinkChannel.write(buffer);
+						while(buffer.hasRemaining()) { // checks if there is enough space in the buffer
+							sinkChannel.write(buffer); // writes the buffer into the sink channel
 						}
 						buffer.flip();
-						Thread.sleep(100);
+						Thread.sleep(100); // sleep so the source channel (destination) can access the pipe
 					}
 
 				} catch(Exception e) {
@@ -34,17 +34,17 @@ public class MainPipe {
 			
 			Runnable reader = () -> {
 				try {
-					Pipe.SourceChannel sourceChannel = pipe.source();
-					ByteBuffer buffer = ByteBuffer.allocate(56);
+					Pipe.SourceChannel sourceChannel = pipe.source(); // SourceChannel is TO where we send the data
+					ByteBuffer buffer = ByteBuffer.allocate(56); // same buffer
 					
 					for(int i=0; i<10; i++) {
-						int bytesRead = sourceChannel.read(buffer);
-						byte[] timeString = new byte[bytesRead];
+						int bytesRead = sourceChannel.read(buffer); // returns how many bytes are in the buffer filled from the source channel
+						byte[] timeString = new byte[bytesRead]; // creates the size of byte[] to hold the elements of the buffer
 						buffer.flip();
-						buffer.get(timeString);
-						System.out.println("Reader Thread: "+ new String(timeString));
+						buffer.get(timeString); // writes the buffer TO timeString byte array
+						System.out.println("Reader Thread: "+ new String(timeString)); // prints the byte array
 						buffer.flip();
-						Thread.sleep(100);
+						Thread.sleep(100); // the sink channel has access to the pipe
 					}
 					
 				} catch(Exception e) {
@@ -52,6 +52,7 @@ public class MainPipe {
 				}
 			};
 			
+			// we run the 2 threads
 			new Thread(writer).start();
 			new Thread(reader).start();
 
